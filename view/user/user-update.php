3fd6cf540e059ec0e9083b226f076745f3d16de7
Oleksandr Klochko <utoyvo@protonmail.com>
2019-04-20 17:34:26 +0300

    Create user-update.php

---
+++ view/user/user-update.php
@@ -0,0 +1,32 @@
+<?php include( '/view/template/header.php' ); ?>
+
+	<section class="card mt-3 mb-3">
+		<header class="card-header">
+			<h1 class="mb-0"><?= htmlentities( $title ); ?></h1>
+		</header>
+
+		<div class="card-body">
+			<?php if ( $errors ) : ?>
+				<div class="alert alert-danger">
+					<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<?php foreach ( $errors as $field => $error ) echo $error . '<br>'; ?>
+				</div>
+			<?php endif; ?>
+
+			<form action="" method="POST">
+				<div class="form-group">
+					<label for="name">Name <span style="color:#f00">*</span></label>
+					<input type="text" name="name" id="name" class="form-control" value="<?= $user->user_name; ?>" required>
+				</div>
+				<div class="form-group">
+					<label for="email">Email <span style="color:#f00">*</span></label>
+					<input type="email" name="email" id="email" class="form-control" value="<?= $user->user_email; ?>" required>
+				</div>
+				<input type="hidden" name="id" id="id" value="<?= $user->user_id; ?>">
+				<button type="submit" name="update" id="submit" class="btn btn-primary">Update</button>
+				<a href="index.php" class="btn btn-light">Cancel</a>
+			</form>
+		</div>
+	</section>
+
+<?php include( '/view/template/footer.php' ); ?>
