327affbc088c6ab4df0a8a0f8259d52dbd5e8502
Oleksandr Klochko <utoyvo@protonmail.com>
2019-04-20 15:21:50 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Config
+ */
+return array(
+	'db_driver'   => 'mysql',
+	'db_server'   => 'localhost',
+	'db_name'     => 'crud-mvc-oop-pdo',
+	'db_username' => 'root',
+	'db_password' => '',
+);
