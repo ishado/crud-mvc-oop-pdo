3f80adb240a9409cec041e315ffca5c79ff88b61
Oleksandr Klochko <utoyvo@protonmail.com>
2019-04-20 16:46:43 +0300

    Create error.php

---
+++ view/error/error.php
@@ -0,0 +1,10 @@
+<?php include( '/view/template/header.php' ); ?>
+
+<div class="card mt-3 mb-3">
+	<div class="card-body">
+		<h1 class="card-title"><?= htmlentities( $title ); ?></h1>
+		<p class="card-text"><?= $message; ?></p>
+	</div>
+</div>
+
+<?php include( '/view/template/footer.php' ); ?>
